dbd811e5f6055d395ca1192e455281fc8a74732c
amontalvof <amontalvof92@gmail.com>
2023-04-07 00:09:31 -0400

    feat: uiReducer

--- src/context/ui/uiReducer.ts
+++ src/context/ui/uiReducer.ts
@@ -1,3 +1,17 @@
-export const uiReducer = (state) => {
-    return state;
+import { UIState } from './UIProvider';
+
+type UIActionType = {
+    type: 'UI - Open Sidebar' | 'UI - Close Sidebar';
+    payload: any;
+};
+
+export const uiReducer = (state: UIState, action: UIActionType): UIState => {
+    switch (action.type) {
+        case 'UI - Open Sidebar':
+            return { ...state, sideMenuOpen: true };
+        case 'UI - Close Sidebar':
+            return { ...state, sideMenuOpen: false };
+        default:
+            return state;
+    }
 };
